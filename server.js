f6eea4d662e6d9cfb97d19e9c53bb0493c95f844
Hariharan Ravichandran <prethikaam@gmail.com>
2025-02-10 19:57:39 +0000

    graphl set up

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
-const typeDefs = "";
+const users = [
+  { id: "1", name: "John Doe", age: 30, isMarried: true },
+  { id: "2", name: "Jane Smith", age: 25, isMarried: true },
+  { id: "3", name: "Alice Johnson", age: 15, isMarried: false },
+];
+
+const typeDefs = `
+type Query {
+getUsers: [User]
+getUserById (id:ID!): User
+
+}
+
+type Mutation {
+createUser(name: String!, age: Int!, isMarried:Boolean!):User
+}
+
+type User{
+id: ID
+name : String
+age: Int
+isMarried: Boolean
+
+}
+
+`;
 const resolvers = {};
 const server = new ApolloServer({ typeDefs, resolvers });
 
